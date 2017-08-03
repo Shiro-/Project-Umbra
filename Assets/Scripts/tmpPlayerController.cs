a50ef254abd367f79a700b81934e787265e3d06a
Noel Eugenio <noel.shirodev@gmail.com>
2017-08-03 13:02:52 -0400

    Add:
    Added some temporary objects for test purposes. Might not work as expected.

---
+++ Assets/Scripts/tmpPlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tmpPlayerController : MonoBehaviour
+{
+    //We need rigidbody for movement
+    private Rigidbody rb;
+
+    public float spd;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Updates every frame
+    void Update()
+    {
+        
+    }
+
+    //Called before Physics step
+    void FixedUpdate()
+    {
+        //Getting inputs
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0.0f, moveZ);
+        rb.AddForce(movement * spd);
+    }
+}
