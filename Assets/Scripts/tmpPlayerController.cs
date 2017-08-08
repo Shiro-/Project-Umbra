964268aa6137e3d870a86ed16ad096a1643ee514
Unknown <jaybritchie@gmail.com>
2017-08-07 20:47:03 -0400

    Mouse Aim/Rotation
    Player cube now rotates to face mouse position - no controller aim yet

--- Assets/Scripts/tmpPlayerController.cs
+++ Assets/Scripts/tmpPlayerController.cs
@@ -28,6 +28,18 @@ public class tmpPlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
+        //Mouse aim/rotation
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane mousePlane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        if (mousePlane.Raycast(ray, out distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 dir = target - transform.position;
+            float rotation = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            rb.transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }
+
         Vector3 movement = new Vector3(moveX, 0.0f, moveZ);
         rb.AddForce(movement * spd);
     }
