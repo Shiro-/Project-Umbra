14852df53b65624e6232e7135f7a1bf2c17c07f0
Noel Eugenio <noel.shirodev@gmail.com>
2017-09-04 16:53:55 -0400

    Enemy Spawner
    Made a new prefab for testing. Currently the spawner works but it spawns below the floor for some reason.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject ghost;
+    public int ghostCount;
+    public float waitTime;
+    public float spawnTime;
+    public Vector3 spawnPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    //https://docs.unity3d.com/Manual/InstantiatingPrefabs.html
+    IEnumerator SpawnEnemies()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        while(true)
+        {
+            for (int i = 0; i < ghostCount; i++)
+            {
+                Instantiate(ghost, new Vector3(Random.Range(-spawnPosition.x, spawnPosition.x), 1, Random.Range(-spawnPosition.z, spawnPosition.z)), ghost.transform.rotation);
+                yield return new WaitForSeconds(spawnTime);
+            }
+            break;
+        }
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
