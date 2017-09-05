d2017db41b64fbc901b1df8f4a54847e1bc3e826
Noel Eugenio <noel.shirodev@gmail.com>
2017-09-05 00:58:45 -0400

    Changed enemy spawner
    Instead of taking one prefab, it now takes an array of prefabs and chooses which one to spawn by random for now.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-
-    public GameObject ghost;
+    //Array enemies
+    public GameObject[] enemies;
     public Light winLight;
-    public int ghostCount;
+
+    public int enemyCount;
+
     public float waitTime;
     public float spawnTime;
+
     public Vector3 spawnPosition;
 
     private int check;
@@ -29,26 +32,21 @@ public class GameController : MonoBehaviour
 
         while(true)
         {
-            for (int i = 0; i < ghostCount; i++)
+            for (int i = 0; i < enemyCount; i++)
             {
-                Instantiate(ghost, new Vector3(Random.Range(-spawnPosition.x, spawnPosition.x), spawnPosition.y, Random.Range(-spawnPosition.z, spawnPosition.z)), ghost.transform.rotation);
+                //Random ranges lol good joke
+                Instantiate(enemies[Random.Range(0, enemies.Length)], new Vector3(Random.Range(-spawnPosition.x, spawnPosition.x), spawnPosition.y, Random.Range(-spawnPosition.z, spawnPosition.z)), Quaternion.identity);
                 yield return new WaitForSeconds(spawnTime);
 
                 check++;
                 print(check);
             }
 
-            if (check == ghostCount)
+            if (check == enemyCount)
             {
                 winLight.enabled = true;
                 break;
             }
         }
     }
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
 }
