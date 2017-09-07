df45a21fd42d757e731dfa7b5b09a03fc5f57e08
Noel Eugenio <noel.shirodev@gmail.com>
2017-09-07 17:43:52 -0400

    Added Dummy Controller Collision
    When the player dies, the dummy controller will destroy itself

--- Assets/Scripts/DummyPlayerController.cs
+++ Assets/Scripts/DummyPlayerController.cs
@@ -11,8 +11,19 @@ public class DummyPlayerController : MonoBehaviour
 
     }
 
+    void Update()
+    {
+        //If the player dies destroy the dummy controller
+        if (GameObject.FindWithTag("Player") == null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void FixedUpdate()
     {
+        //The dummy controller will handle the movement 
+        //While the player itself will controller the rotation
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
         transform.Translate(moveX, 0.0f, moveZ);
