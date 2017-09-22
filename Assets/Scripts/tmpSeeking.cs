9447be926d636394cadb5e93f04c49f0810a0fb7
Noel Eugenio <noel.shirodev@gmail.com>
2017-09-21 20:46:34 -0400

    Added Seeking script for testing
    Removed the walls for now.
    Added a new prefab for testing seeking(Not finished yet).
    Updated Scene.

---
+++ Assets/Scripts/tmpSeeking.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tmpSeeking : MonoBehaviour
+{
+    //Seek target
+    public GameObject player;
+
+    //Enemy properties
+    public float spd;
+    public float min;
+    public float max;
+
+    private void Update()
+    {
+        //Check if player is within distance of enemy
+        //if()
+    }
+
+}
