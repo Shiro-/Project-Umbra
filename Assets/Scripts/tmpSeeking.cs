8880a681a70f4d88072720a3e2b2bd09b8d1190e
Noel Eugenio <noel.shirodev@gmail.com>
2017-09-22 16:36:22 -0400

    Seeking - More stuff
    Not working yet

--- Assets/Scripts/tmpSeeking.cs
+++ Assets/Scripts/tmpSeeking.cs
@@ -10,12 +10,41 @@ public class tmpSeeking : MonoBehaviour
     //Enemy properties
     public float spd;
     public float min;
-    public float max;
+    //public float moveX;
+    //public float moveZ;
+    //public float max;
 
-    private void Update()
+    private bool chase;
+
+    void Start()
     {
-        //Check if player is within distance of enemy
-        //if()
+        chase = !chase;
     }
 
+    void Update()
+    {
+        //Not Tested yet
+        //Check if player is within distance of enemy
+        //Check if chasing is true
+        //Check if the object found is the Player
+        //https://docs.unity3d.com/ScriptReference/Vector3.Distance.html
+        //if (transform.position.z <= min && chase == false && GameObject.FindWithTag("Player") != null)
+        if (Vector3.Distance(transform.position, player.transform.position) >= min && chase == false && GameObject.FindWithTag("Player") != null)
+        {
+            //If the enemy is within the minimum dist from the player
+            //Look at the player and start moving towards them
+            transform.LookAt(player.transform);
+            chase = !chase;
+        }
+
+        if (chase == true)
+        {
+            transform.position += transform.forward * spd;
+        }
+        else
+        {
+            //transform.position(Random.RandomRange(), 0.0f, )
+        }
+
+    }
 }
