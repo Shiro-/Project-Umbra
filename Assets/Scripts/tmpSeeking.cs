2ed4ec5f087cb6bc1be8bb979bb977b4935e7371
Noel Eugenio <noel.shirodev@gmail.com>
2017-09-22 16:52:36 -0400

    Seeking - starting over
    Added the nose 👃👃👃

--- Assets/Scripts/tmpSeeking.cs
+++ Assets/Scripts/tmpSeeking.cs
@@ -29,22 +29,27 @@ public class tmpSeeking : MonoBehaviour
         //Check if the object found is the Player
         //https://docs.unity3d.com/ScriptReference/Vector3.Distance.html
         //if (transform.position.z <= min && chase == false && GameObject.FindWithTag("Player") != null)
-        if (Vector3.Distance(transform.position, player.transform.position) >= min && chase == false && GameObject.FindWithTag("Player") != null)
-        {
-            //If the enemy is within the minimum dist from the player
-            //Look at the player and start moving towards them
-            transform.LookAt(player.transform);
-            chase = !chase;
-        }
+        //if (Vector3.Distance(transform.position, player.transform.position) >= min && chase == false && GameObject.FindWithTag("Player") != null)
+        //{
+        //    //If the enemy is within the minimum dist from the player
+        //    //Look at the player and start moving towards them
+        //    transform.LookAt(player.transform);
+        //    chase = !chase;
+        //}
 
-        if (chase == true)
-        {
-            transform.position += transform.forward * spd;
-        }
-        else
-        {
-            //transform.position(Random.RandomRange(), 0.0f, )
-        }
+        //if (chase == true)
+        //{
+        //    transform.position += transform.forward * spd;
+        //}
+        //else
+        //{
+        //    //transform.position(Random.RandomRange(), 0.0f, )
+        //}
+        if (GameObject.FindWithTag("Player") != null)
+            transform.LookAt(player.transform);
+
+        if (Vector3.Distance(transform.position, player.transform.position) <= min)
+            Debug.Log("beeb");
 
     }
 }
