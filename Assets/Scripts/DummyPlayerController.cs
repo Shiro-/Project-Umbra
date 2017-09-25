c7348503676c3d7f5a6ac67f5599e0020adfd13b
Noel Eugenio <noel.shirodev@gmail.com>
2017-09-25 17:54:49 -0400

    Changed dummy controller
    Testing purposes

--- Assets/Scripts/DummyPlayerController.cs
+++ Assets/Scripts/DummyPlayerController.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class DummyPlayerController : MonoBehaviour
 {
+    //Testing, subject to change
+    public float spd;
+
     void Update()
     {
-        //The dummy controller will handle the movement 
-        //While the player itself will controller the rotation
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-        transform.Translate(moveX, 0.0f, moveZ);
-
         //If the player dies destroy the dummy controller
         if (GameObject.FindWithTag("Player") == null)
         {
             Destroy(gameObject);
         }
+    }
+    void FixedUpdate()
+    {
+        //The dummy controller will handle the movement 
+        //While the player itself will controller the rotation
+        float moveX = Input.GetAxis("Horizontal") * spd * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * spd * Time.deltaTime;
+        transform.Translate(moveX, 0.0f, moveZ);
     }
 }
