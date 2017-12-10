7ce8abb5578343687ed49fcd016f1d3b1e9bd023
Noel Eugenio <noel.shirodev@gmail.com>
2017-12-10 18:13:41 -0500

    Creating wall boundary
    Not working yet

--- Assets/Scripts/DummyPlayerController.cs
+++ Assets/Scripts/DummyPlayerController.cs
@@ -7,6 +7,9 @@ public class DummyPlayerController : MonoBehaviour
     //Testing, subject to change
     public float spd;
 
+    private float moveX;
+    private float moveZ;
+
     void Update()
     {
         //If the player dies destroy the dummy controller
@@ -15,12 +18,22 @@ public class DummyPlayerController : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     void FixedUpdate()
     {
         //The dummy controller will handle the movement 
         //While the player itself will controller the rotation
-        float moveX = Input.GetAxis("Horizontal") * spd * Time.deltaTime;
-        float moveZ = Input.GetAxis("Vertical") * spd * Time.deltaTime;
+        moveX = Input.GetAxis("Horizontal") * spd * Time.deltaTime;
+        moveZ = Input.GetAxis("Vertical") * spd * Time.deltaTime;
         transform.Translate(moveX, 0.0f, moveZ);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Environment")
+        {
+            moveX = 0.0f;
+            moveZ = 0.0f;
+        }
     }
 }
