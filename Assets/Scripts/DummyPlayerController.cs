8d4eb06677b920f77db632331e7d27c52d6a2c61
Noel Eugenio <noel.shirodev@gmail.com>
2017-12-10 19:02:05 -0500

    Fixed boundary
    https://docs.unity3d.com/ScriptReference/Mathf.Clamp.html

--- Assets/Scripts/DummyPlayerController.cs
+++ Assets/Scripts/DummyPlayerController.cs
@@ -26,14 +26,22 @@ public class DummyPlayerController : MonoBehaviour
         moveX = Input.GetAxis("Horizontal") * spd * Time.deltaTime;
         moveZ = Input.GetAxis("Vertical") * spd * Time.deltaTime;
         transform.Translate(moveX, 0.0f, moveZ);
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Environment")
-        {
-            moveX = 0.0f;
-            moveZ = 0.0f;
-        }
+        //Boundary check
+        /*
+         * In simplest terms, This will prevent the player from leaving the play area
+         * AKA, the size of the purple floor.
+         * 
+         * There are other ways to create this solution such as checking for collisions with walls.
+         * But since our player is a trigger, we can't use that method.
+         * 
+         * So while we have walls, they are mostly decoration since they won't do anything.
+         * 
+         * https://docs.unity3d.com/ScriptReference/Mathf.Clamp.html
+         */
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(position.x + moveX, -14.0f, 14.0f);
+        position.z = Mathf.Clamp(position.z + moveZ, -14.0f, 14.0f);
+        transform.position = position;
     }
 }
