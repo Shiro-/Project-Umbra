d2e86da5e1d240116bd3a4aa226ec3ad5b8eb089
Unknown <jaybritchie@gmail.com>
2017-12-11 20:17:34 -0500

    Starting work on doors
    Just a basic automatic open at the moment because it's time to eat dinner

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Transform t;
+    private GameObject player;
+
+    [Tooltip("Rotation value to open the door.")]
+    public float openR;
+
+    // Use this for initialization
+    void Start()
+    {
+        t = GetComponent<Transform>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //For now we will just destroy ourselves and the enemy
+        //for future reference we will have hp and other things
+        if (other.tag == "Player")
+        {
+            t.rotation = Quaternion.Euler(0, openR, 0);
+        }
+        //Changed tags to prepare for different enemies
+        //and also hp values for the player
+    }
+}
